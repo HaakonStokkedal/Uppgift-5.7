e925828bf75d38f77d42a0528519befb6b054032
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2023-11-26 19:42:10 +0100

    Add project files.

---
+++ Uppgift 5.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_5._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] länder = new string[5];
+
+            länder[0] = "Kina";
+            länder[1] = "Indien";
+            länder[2] = "USA";
+            länder[3] = "Indonesien";
+            länder[4] = "Pakistan";
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i + 1}. {länder[i]}");
+            }
+            Console.WriteLine("Vilka siffror ska visas igen? (sätt mellanrum mellan siffrorna)");
+            string siffror = Console.ReadLine();
+            string[] Siffror = siffror.Split(' ');
+            foreach(string siffraText  in Siffror)
+            {
+                int siffra = int.Parse(siffraText);
+                Console.WriteLine($"{siffra}. {länder[siffra - 1]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
